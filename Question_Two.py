63eab5139659bb1cc79c8934bb42b01f0f2b7605
rajcaptainindia <rajcsecr@gmail.com>
2020-04-22 17:45:07 +0530

    Update and rename Question2.py to Question_Two.py

--- Question_Two.py
+++ Question_Two.py
@@ -8,7 +8,6 @@ def list_to_num(sub_list):
     return res
     
 def solution(list1):
-    #sub_list=[]
     res=[]
     for i in range(len(list1)):
         for j in range(len(list1)):
@@ -16,13 +15,11 @@ def solution(list1):
                 if(i!=j and j!=k and k!=i):
                     sub_list=[list1[i],list1[j],list1[k]]
                     res.append(list_to_num(sub_list))
-    #print(set(res))
     unique_res_list=list(set(res))
-    print(max(unique_res_list),len(unique_res_list))
+    print(max(unique_res_list),len(unique_res_list),sep=",")
     
-    
+# INPUT     
 list1=[1,2,1,4]
 solution(list1)
-#print(output)
 
 
