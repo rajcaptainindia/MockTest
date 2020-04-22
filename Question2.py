025cc2f4cc18b0b4756292431bdbc6937c840312
rajcaptainindia <rajcsecr@gmail.com>
2020-04-22 17:33:15 +0530

    Create Question2.py

---
+++ Question2.py
@@ -0,0 +1,28 @@
+#jai Ganesha
+
+def list_to_num(sub_list):
+    res=""
+    for i in sub_list:
+        res+=str(i)
+    #print(res)
+    return res
+    
+def solution(list1):
+    #sub_list=[]
+    res=[]
+    for i in range(len(list1)):
+        for j in range(len(list1)):
+            for k in range(len(list1)):
+                if(i!=j and j!=k and k!=i):
+                    sub_list=[list1[i],list1[j],list1[k]]
+                    res.append(list_to_num(sub_list))
+    #print(set(res))
+    unique_res_list=list(set(res))
+    print(max(unique_res_list),len(unique_res_list))
+    
+    
+list1=[1,2,1,4]
+solution(list1)
+#print(output)
+
+
